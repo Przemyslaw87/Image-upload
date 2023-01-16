bc84427585ae2008ac9059ee2d78f062c41715a2
PrzemPawelek <przemyslaw.pawelek@stxnext.pl>
2023-01-16 12:54:49 +0100

    Fix Secrete

--- imageproject/settings.py
+++ imageproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d51-b-o-k-5zts=agf*5a9^xy4q1#mdvmp93(=r^&)-z6c-8!y'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
